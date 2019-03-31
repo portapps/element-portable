9d077c4d8880587972095c37edd576e3c8f460b1
CrazyMax <crazy-max@users.noreply.github.com>
2019-03-31 16:56:51 +0200

    Initial version based on Riot 1.0.5 (portapps/portapps#10)

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/portapps/riot-portable
+
+require (
+	github.com/kevinburke/go-bindata v3.13.0+incompatible
+	github.com/portapps/portapps v1.21.0
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+//go:generate go install -v github.com/kevinburke/go-bindata/go-bindata
+//go:generate go-bindata -prefix res/ -pkg assets -o assets/assets.go res/Riot.lnk
+//go:generate go install -v github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+//go:generate goversioninfo -icon=res/papp.ico
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+
+	_ "github.com/kevinburke/go-bindata"
+	. "github.com/portapps/portapps"
+	"github.com/portapps/portapps/pkg/shortcut"
+	"github.com/portapps/portapps/pkg/utl"
+	"github.com/portapps/riot-portable/assets"
+)
+
+var (
+	app *App
+)
+
+func init() {
+	var err error
+
+	// Init app
+	if app, err = New("riot-portable", "Riot"); err != nil {
+		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
+	}
+}
+
+func main() {
+	utl.CreateFolder(app.DataPath)
+	electronBinPath := utl.PathJoin(app.AppPath, utl.FindElectronAppFolder("app-", app.AppPath))
+
+	app.Process = utl.PathJoin(app.AppPath, "Riot.exe")
+	app.WorkingDir = electronBinPath
+
+	// Copy default shortcut
+	shortcutPath := path.Join(utl.StartMenuPath(), "Riot Portable.lnk")
+	defaultShortcut, err := assets.Asset("Riot.lnk")
+	if err != nil {
+		Log.Error().Err(err).Msg("Cannot load asset Riot.lnk")
+	}
+	err = ioutil.WriteFile(shortcutPath, defaultShortcut, 0644)
+	if err != nil {
+		Log.Error().Err(err).Msg("Cannot write default shortcut")
+	}
+
+	// Update default shortcut
+	err = shortcut.Create(shortcut.Shortcut{
+		ShortcutPath:     shortcutPath,
+		TargetPath:       app.Process,
+		Arguments:        shortcut.Property{Clear: true},
+		Description:      shortcut.Property{Value: "Riot Portable by Portapps"},
+		IconLocation:     shortcut.Property{Value: app.Process},
+		WorkingDirectory: shortcut.Property{Value: app.AppPath},
+	})
+	if err != nil {
+		Log.Error().Err(err).Msg("Cannot create shortcut")
+	}
+	defer func() {
+		if err := os.Remove(shortcutPath); err != nil {
+			Log.Error().Err(err).Msg("Cannot remove shortcut")
+		}
+	}()
+
+	app.Launch(os.Args[1:])
+}
